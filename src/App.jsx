0e6397a4459c9cc2380c01b9600868531af87cfa
Trần Khánh Gia Uy - 18126011 <uytran36@gmail.com>
2021-12-23 09:54:32 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Main from "./pages/MainPage";
+import { useState, useEffect } from "react";
+
+function App() {
+  return (
+    <div>
+      <Main />
+    </div>
+  );
+}
+
+export default App;
