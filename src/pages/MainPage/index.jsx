3e0982ba7379373380bdd92ba34d3f02b8cc1eb3
Trần Khánh Gia Uy - 18126011 <uytran36@gmail.com>
2022-01-04 22:38:56 +0700

    update

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,6 +1,5 @@
-import { Layout } from "antd";
+import { Layout, Avatar, Row, Col } from "antd";
 import "./style.css";
-import { Avatar } from "antd";
 import { HomeTwoTone, AntDesignOutlined } from "@ant-design/icons";
 
 const { Header, Content, Footer } = Layout;
@@ -10,21 +9,39 @@ const HomePage = () => {
     <>
       <Layout className="layout">
         <Header style={{ backgroundColor: "white" }}>
-          <div className="home">
-            <Avatar shape="square" icon={<HomeTwoTone />} />
-          </div>
-          <div className="story">Câu chuyện cà phê xưa</div>
-          <div className="traditional">Cà phê xưa dân gian</div>
-          <div className="logo">
-            <Avatar
-              size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
-              icon={<AntDesignOutlined />}
-            />
-          </div>
-          <div className="garden">Khu vườn cà phê</div>
-          <div className="knowledge">Kiến thức cà phê</div>
-          <div className="system">Hệ thống</div>
-          <div className="contact">Liên hệ</div>
+          <Row>
+            <Col span="1">
+              <div className="home">
+                <Avatar shape="square" icon={<HomeTwoTone />} />
+              </div>
+            </Col>
+            <Col span="3">
+              <div className="story">Câu chuyện cà phê xưa</div>
+            </Col>
+            <Col span="3">
+              <div className="traditional">Cà phê xưa dân gian</div>
+            </Col>
+            <Col span="3">
+              <div className="garden">Khu vườn cà phê</div>
+            </Col>
+            <Col span="5">
+              <div className="logo">
+                <Avatar
+                  size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+                  icon={<AntDesignOutlined />}
+                />
+              </div>
+            </Col>
+            <Col span="3">
+              <div className="knowledge">Kiến thức cà phê</div>
+            </Col>
+            <Col span="3">
+              <div className="system">Hệ thống</div>
+            </Col>
+            <Col span="3">
+              <div className="contact">Liên hệ</div>
+            </Col>
+          </Row>
         </Header>
         <Content style={{ padding: "0 50px" }}></Content>
         <Footer></Footer>
